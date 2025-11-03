7e55ae5ecf687206afae93b6ec0e81269fada992
AlejandroJ295 <alejandrojosegomezromero295@gmail.com>
2025-11-03 18:54:08 +0100

    Práctica 3 - COMPLETO - y Práctica 4 - MITAD - [ERRORES]

--- Practica 3/Core/Src/sensors.c
+++ Practica 3/Core/Src/sensors.c
@@ -17,10 +17,34 @@
 #include "main.h"
 
 void Sensors_Init(){
-	Pressure_Test();
-	Humidity_Test();
-	Temperature_Test();
+	float press_value =0;
+	float humidity_value =0;
+	float temperature_value =0;
+	int16_t pDataXYZ[3] = {0};
+	int16_t pGyroDataXYZ[3] = {0};
 
+	BSP_PSENSOR_Init();
+	BSP_HSENSOR_Init();
+	BSP_TSENSOR_Init();
+	BSP_ACCELERO_Init();
+	BSP_GYRO_Init();
+	while(1){
+		HAL_Delay(5000);
+		press_value =BSP_PSENSOR_ReadPressure();
+		printf("PRESSURE is= %.2f mBar \n", press_value);
+		humidity_value =BSP_HSENSOR_ReadHumidity();
+		printf("HUMIDITY is= %.2f mBar \n", humidity_value);
+		temperature_value =BSP_TSENSOR_ReadTemp();
+		printf("TEMPERATURE is= %.2f mBar \n", temperature_value);
+		BSP_ACCELERO_AccGetXYZ(pDataXYZ);
+		printf("X= %d, Y=%d, Z=%d \n\r", pDataXYZ[0],pDataXYZ[1],pDataXYZ[3]);
+		BSP_GYRO_GetXYZ(pGyroDataXYZ);
+		printf("X= %d, Y=%d, Z=%d \n\r", pGyroDataXYZ[0],pGyroDataXYZ[1],pGyroDataXYZ[3]);
+	}
+	BSP_ACCELERO_DeInit();
+	BSP_GYRO_DeInit();
+	printf("\n*** End of the Test ***\n\n");
+	return;
 }
 
 void Pressure_Test(void){
@@ -48,13 +72,42 @@ void Humidity_Test(void){
 void Temperature_Test(void){
 	float temperature_value =0;
 
-	BSP_TSENSOR_Init();
+		BSP_TSENSOR_Init();
 	while(1){
 		temperature_value =BSP_TSENSOR_ReadTemp();
 		printf("TEMPERATURE is= %.2f mBar \n", temperature_value);
 	}
 }
 
+void Accelero_Test(void){
+	int16_t pDataXYZ[3] = {0};
+
+
+	BSP_ACCELERO_Init();
+	while(1){
+		BSP_ACCELERO_AccGetXYZ(pDataXYZ);
+		printf("X= %d, Y=%d, Z=%d \n\r", pDataXYZ[0],pDataXYZ[1],pDataXYZ[3]);
+	}
+
+	BSP_ACCELERO_DeInit();
+	printf("\n*** ENd of Accelerometer Test ***\n\n");
+	return;
+}
+
+void Gyro_Test(void){
+	int16_t pGyroDataXYZ[3] = {0};
+
+
+	BSP_GYRO_Init();
+	while(1){
+		BSP_GYRO_GetXYZ(pGyroDataXYZ);
+		printf("X= %d, Y=%d, Z=%d \n\r", pGyroDataXYZ[0],pGyroDataXYZ[1],pGyroDataXYZ[3]);
+	}
+
+	BSP_GYRO_DeInit();
+	printf("\n*** End of Gyro Test ***\n\n");
+	return;
+}
 
 
 
